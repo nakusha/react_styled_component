341be2932696bdd2fb0d00b7711800ff1867e40c
정연수 <jys1015@hanyang.co.kr>
2019-10-22 17:09:16 +0900

    2. styled Component

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 react 앱 처음 만들기
 	npm npx 인스톨
 		npx create-react-app 폴더명
+
+npm install styled-components
--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
 import React, {Component, Fragment } from 'react';
-import "./App.css";
+import styled from "styled-components";
 
 class App extends Component{
   render() { 
     return(
-      <Fragment>
-        <Button danger />
-        <Button />
-      </Fragment>
+      <Container>
+        <Button danger>Hello</Button>
+        <Button>Hello</Button>
+      </Container>
     );
   }
 }
 
-const Button = ({danger}) => (
-  <Button className={danger ? "button button--danger" : "button button--success" }>
-    Hello
-  </Button>
-);
+// 스타일하려는 html 요소르 럲는다.
+
+const Container = styled.div`
+  height:100vh;
+  widht:100%;
+  background-color:#bdc3c7;
+`
+const Button = styled.button`
+  border-radius: 50px;
+  padding: 5px;
+  min-width: 120px;
+  color: white;
+  font-weight: 600;
+  -webkit-appearance: none;
+  cursor: pointer;
+  &:active,
+  &:focus {
+    outline: none;
+  }
+  background-color:${props => props.danger ? "#e74c3c" : "#2ecc71"}
+`
 export default App;
